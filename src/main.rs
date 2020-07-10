c18163718d22c4e194f537a777a755275dfeae01
Yishen Miao <mys721tx@gmail.com>
2020-07-10 15:33:29 -0700

    Handling frame
    Handles frame.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::io;
 use std::process;
 
 use serde::de::{self, Visitor};
+use serde::ser;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
 use csv::ReaderBuilder;
@@ -150,6 +151,57 @@ mod serde_score {
     impl std::error::Error for ScoreError {}
 }
 
+mod serde_frame {
+    use super::*;
+
+    struct FrameVisitor;
+
+    impl<'de> Visitor<'de> for FrameVisitor {
+        type Value = Option<u64>;
+
+        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+            formatter.write_str("0, 1, 2 or a dot")
+        }
+
+        fn visit_char<E>(self, value: char) -> Result<Self::Value, E>
+        where
+            E: de::Error,
+        {
+            match value {
+                '0' => Ok(Some(0)),
+                '1' => Ok(Some(1)),
+                '2' => Ok(Some(2)),
+                '.' => Ok(None),
+                _ => Err(E::custom(format!(
+                    "invalid character {:?} in the frame",
+                    value
+                ))),
+            }
+        }
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Option<u64>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_char(FrameVisitor)
+    }
+
+    pub fn serialize<S>(strand: &Option<u64>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match *strand {
+            Some(v) => if 0 < v && v < 3 {
+                serializer.serialize_u64(0)
+            } else {
+                Err(ser::Error::custom(format!("invalid frame {}", v)))
+            },
+            None => serializer.serialize_char('.'),
+        }
+    }
+}
+
 #[derive(Debug, Deserialize, Serialize)]
 struct Record {
     seqname: String,
@@ -161,7 +213,8 @@ struct Record {
     score: Option<f64>,
     #[serde(with = "serde_strand")]
     strand: Option<Strand>,
-    frame: String,
+    #[serde(with = "serde_frame")]
+    frame: Option<u64>,
     attributes: String,
 }
 
@@ -174,6 +227,7 @@ fn reader() -> Result<(), Box<dyn Error>> {
     let mut rdr = ReaderBuilder::new()
         .delimiter(b'\t')
         .has_headers(false)
+        .comment(Some(b'#'))
         .from_reader(GFF_FILE);
     for result in rdr.deserialize() {
         let record: Record = result?;
@@ -192,7 +246,7 @@ fn writer() -> Result<(), Box<dyn Error>> {
             end: 1,
             score: None,
             strand: Some(Strand::Forward),
-            frame: ".".to_owned(),
+            frame: None,
             attributes: "Note=Removed,Obsolete;ID=test".to_owned(),
         },
         Record {
@@ -203,7 +257,7 @@ fn writer() -> Result<(), Box<dyn Error>> {
             end: 176,
             score: Some(50.0),
             strand: Some(Strand::Forward),
-            frame: ".".to_owned(),
+            frame: None,
             attributes: "Note=ATP-dependent protease subunit HslV;ID=PRO_0000148105".to_owned(),
         },
     ];
